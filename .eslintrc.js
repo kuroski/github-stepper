da79a4a6dfaebbc87a312b5f6cb57c0d9acd7923
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-05-30 18:48:21 +0200

    Add testing-library-jest-dom dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/recommended",
     "plugin:vue/essential",
     "eslint:recommended",
-    "@vue/prettier"
+    "@vue/prettier",
   ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "no-underscore-dangle": ["error", { allowAfterThis: true }]
+    "no-underscore-dangle": ["error", { allowAfterThis: true }],
   },
   overrides: [
     {
       files: [
         "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
+        jest: true,
+      },
+    },
   ],
-  plugins: ["testing-library"]
+  plugins: ["testing-library"],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: "@vue/cli-plugin-unit-jest",
+  setupFilesAfterEnv: ["./tests/unit/setup.js"],
+  moduleNameMapper: {
+    "@unit(.*)$": "<rootDir>/tests/unit/$1"
+  }
 };
