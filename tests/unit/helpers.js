76b319cc1aec0447801280f6c0a30cf07aba25c4
Daniel Kuroski <daniel.kuroski@gmail.com>
2020-05-31 21:56:53 +0200

    Improved unit/integration test helper to render components

--- tests/unit/helpers.js
+++ tests/unit/helpers.js
@@ -1,4 +1,5 @@
 import { render } from "@testing-library/vue";
+import registerPlugins from "@/plugins";
 import i18n from "@/i18n";
 import routes from "@/router/routes";
 
@@ -6,8 +7,29 @@ export const renderWithDependencies = (
   component,
   options = {},
   callback = a => a
-) =>
-  render(component, { routes, ...options }, vue => ({
-    ...callback(vue),
-    i18n
-  }));
+) => {
+  const mergedOptions = {
+    routes,
+    store: {
+      strict: true,
+      modules: {
+        workflow
+      }
+    },
+    ...options
+  };
+
+  return render(component, mergedOptions, (vue, store, router) => {
+    registerPlugins(store);
+
+    const result = callback(vue, store, router) || {};
+
+    return {
+      vue,
+      store,
+      router,
+      i18n,
+      ...result
+    };
+  });
+};
